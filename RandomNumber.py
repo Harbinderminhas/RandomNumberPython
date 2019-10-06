99bfab99359817c6f6cbe55faf0340e3058a80e1
Harbinderminhas <53470079+Harbinderminhas@users.noreply.github.com>
2019-10-07 01:57:09 +0530

    Add files via upload

---
+++ RandomNumber.py
@@ -0,0 +1,14 @@
+# ************************************
+# Select random number
+
+import random
+
+random_number=random.randint(0, 1)
+# print(random_number)
+
+rand =random.random() * 100
+# print(rand)
+
+lst =["Star Plus","DD1","Aaj Tak","CartoonNetwork","Sony"]
+choice = random.choice(lst)
+print(choice)
